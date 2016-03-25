dbc49dc23e71d5ea19378ab842f1a0c867a7dd0d
AlistairHopkins92 <alistairhopkins92@gmail.com>
2016-03-25 16:14:42 +0000

    first commit with lines working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log("hello")
+$(function(){
+  fillLine()
+});
+
+
+
+
+
+function fillLine(){
+  $('ul .tallrow .narrowcolumn').one("click", function() {
+    $(this).css("background", "black");
+    console.log("boxes to be filled");
+    console.log($(this).val());
+    console.log($(this.id).selector);
+    //console.log($(this).parent().attr("value"));
+  });
+  $('ul .shortrow .widecolumn').one("click", function() {
+    $(this).css("background", "black");
+    console.log("boxes to be filled");
+    console.log($(this).val());
+    console.log($(this.id).selector);
+    //console.log($(this).parent().attr("value"));
+  });
+
+
+
+}
+
+var player1;
+var plater2;
+var whosTurn = [];
+var boxes= [0,0,0,0,0,0,0,0,0]
